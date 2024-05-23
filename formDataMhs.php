de54fb6a2a2bbe1a3c2200e93123a263161c3121
dimdimah <dimahluthfi15@gmail.com>
2024-05-23 22:39:39 +0700

    feat: adding navbar & form data mahasiswa

--- formDataMhs.php
+++ formDataMhs.php
@@ -1,12 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Form Input Data</title>
+</head>
+<body>
+  <h1>Form Input Data</h1>
+  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <label for="nama">Nama:</label>
+    <input type="text" id="nama" name="nama" required><br><br>
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required><br><br>
+
+    <label for="pesan">Pesan:</label>
+    <textarea id="pesan" name="pesan" required></textarea><br><br>
+
+    <input type="submit" value="Kirim">
+  </form>
+
 <?php
-class Mahasiswa(){
-    public $nim;
-    public $nama;
-    public $prodi;
-    public $fakultas;
-    public $asal;
-    public function getDataMhs(){
-        return $this->nim." ".$this->nama." ".$this->prodi." ".$this->fakultas." ".$this->asal;
-    }
+class DataMahasiswa295
+{
+  protected $nama;
+  protected $email;
+  protected $pesan;
+
+  public function __construct($nama, $email, $pesan)
+  {
+    $this->nama = $nama;
+    $this->email = $email;
+    $this->pesan = $pesan;
+  }
+
+  public function getNama295()
+  {
+    return $this->nama;
+  }
+
+  public function getEmail295()
+  {
+    return $this->email;
+  }
+
+  public function getPesan295()
+  {
+    return $this->pesan;
+  }
 }
+
+$data = []; // array kosong untuk menyimpan data
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $nama = $_POST['nama'];
+  $email = $_POST['email'];
+  $pesan = $_POST['pesan'];
+
+  $dataInput = new DataMahasiswa295($nama, $email, $pesan);
+
+  // menyimpan data input ke dalam array kosong
+  $data[] = $dataInput;
+
+  // menampilkan data yang disimpan pada array
+  echo "Data yang Disimpan:<br>";
+  foreach ($data as $inputData) {
+    echo "Nama: " . $inputData->getNama295() . "<br>";
+    echo "Email: " . $inputData->getEmail295() . "<br>";
+    echo "Pesan: " . $inputData->getPesan295() . "<br><br>";
+  }
+}
+
 ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <nav class="navbar bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <a href="?page=form-data-mahasiswa" class="load-content navbar-brand">Load Page 1</a>
+            <a href="?page=form-data-akademik" class="load-content navbar-brand">Load Page 2</a>
+        </div>
+    </nav>
+    <div id="main-content" class="mx-auto p-4">
+        <?php
+        if (isset($_GET['page'])) {
+            $page = $_GET['page'];
+            if ($page == 'form-data-mahasiswa') {
+                include 'formDataMhs.php';
+            } elseif ($page == 'form-data-akademik') {
+                include 'formAkademik.php';
+            } else {
+                echo '<h1>Welcome to the Home Page</h1>';
+                echo '<p>This is the default content.</p>';
+            }
+        } else {
+            echo '<h1>Welcome to the Home Page</h1>';
+            echo '<p>This is the default content.</p>';
+        }
+        ?>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
