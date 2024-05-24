dee70cadb935f50ab6d77c461710948f0577f28f
dimdimah <dimahluthfi15@gmail.com>
2024-05-24 14:24:39 +0700

    feat: footer index.php

--- formAkademik.php
+++ formAkademik.php
@@ -4,73 +4,160 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Form Input Data</title>
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-  <h1>Form Input Data</h1>
-  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    <label for="nama">Nama:</label>
-    <input type="text" id="nama" name="nama" required><br><br>
+  <div class="container mt-5">
+    <h1 class="text-center">Form Akademik Mahasiswa</h1>
+    <form action="<?php echo '?page=form-data-akademik' ?>" method="post" class="mx-auto mt-4" style="max-width: 500px;">
+      <div class="mb-3">
+        <label for="prodi" class="form-label">Program Studi </label>
+        <input type="text" class="form-control" id="prodi" name="prodi296" required>
+      </div>
 
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required><br><br>
+      <div class="mb-3">
+        <label for="dospem" class="form-label">Dosen Pembimbing </label>
+        <input type="text" class="form-control" id="dospem" name="dospem296" required>
+      </div>
 
-    <label for="pesan">Kritik:</label>
-    <textarea id="pesan" name="pesan" required></textarea><br><br>
+      <div class="mb-3">
+        <label for="kelas" class="form-label">Kurikulum Kelas </label>
+        <select class="form-control" id="kelas" name="kelas296" required>
+          <option value="Reguler">Reguler</option>
+          <option value="Non-Reguler">Non-Reguler</option>
+        </select>
+      </div>
+      
+      <div class="mb-3">
+        <label for="angkatan" class="form-label">Angkatan </label>
+        <input type="text" class="form-control" id="angkatan" name="angkatan296" required>
+      </div>
+      
+      <div class="mb-3">
+        <label for="semester" class="form-label">Semester </label>
+        <input type="text" class="form-control" id="semester" name="semester296" required>
+      </div>
+      
+      <div class="mb-3">
+        <label for="jumlah_mk" class="form-label">Jumlah Mata Kuliah yang Diambil </label>
+        <input type="text" class="form-control" id="jumlah_mk" name="jumlah_mk296" required>
+      </div>
+      
+      <div class="mb-3">
+        <label for="ipk" class="form-label">Indeks Prestasi Kumulatif (IPK) </label>
+        <input type="text" class="form-control" id="ipk" name="ipk296" required>
+      </div>
+      
+      <div class="text-center">
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </div>
+    </form>
 
-    <input type="submit" value="Kirim">
-  </form>
+    <?php
+    class akademikMahasiswa296
+    {
+      protected $prodi;
+      protected $angkatan;
+      protected $semester;
+      protected $jumlah_mk;
+      protected $ipk;
 
-<?php
-class DataMahasiswa295
-{
-  protected $nama;
-  protected $email;
-  protected $pesan;
+      public function __construct($prodi, $angkatan, $semester, $jumlah_mk, $ipk)
+      {
+        $this->prodi = $prodi;
+        $this->angkatan = $angkatan;
+        $this->semester = $semester;
+        $this->jumlah_mk = $jumlah_mk;
+        $this->ipk = $ipk;
+      }
 
-  public function __construct($nama, $email, $pesan)
-  {
-    $this->nama = $nama;
-    $this->email = $email;
-    $this->pesan = $pesan;
-  }
+      public function getProdi296()
+      {
+        return $this->prodi;
+      }
 
-  public function getNama295()
-  {
-    return $this->nama;
-  }
+      public function getAngkatan296()
+      {
+        return $this->angkatan;
+      }
 
-  public function getEmail295()
-  {
-    return $this->email;
-  }
+      public function getSemester296()
+      {
+        return $this->semester;
+      }
 
-  public function getPesan295()
-  {
-    return $this->pesan;
-  }
-}
+      public function getJumlahMk296()
+      {
+        return $this->jumlah_mk;
+      }
 
-$data = []; // array kosong untuk menyimpan data
+      public function getIpk296()
+      {
+        return $this->ipk;
+      }
+    }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $nama = $_POST['nama'];
-  $email = $_POST['email'];
-  $pesan = $_POST['pesan'];
+    class detailAkademik296 extends akademikMahasiswa296
+    {
+      protected $kelas;
+      protected $dospem;
 
-  $dataInput = new DataMahasiswa295($nama, $email, $pesan);
+      public function __construct($prodi, $angkatan, $semester, $jumlah_mk, $ipk, $kelas, $dospem)
+      {
+        parent::__construct($prodi, $angkatan, $semester, $jumlah_mk, $ipk);
+        $this->kelas = $kelas;
+        $this->dospem = $dospem;
+      }
 
-  // menyimpan data input ke dalam array kosong
-  $data[] = $dataInput;
+      public function getKelas296()
+      {
+        return $this->kelas;
+      }
 
-  // menampilkan data yang disimpan pada array
-  echo "Data yang Disimpan:<br>";
-  foreach ($data as $inputData) {
-    echo "Nama: " . $inputData->getNama295() . "<br>";
-    echo "Email: " . $inputData->getEmail295() . "<br>";
-    echo "Pesan: " . $inputData->getPesan295() . "<br><br>";
-  }
-}
+      public function getDospem296()
+      {
+        return $this->dospem;
+      }
 
-?>
+    }
+
+    $data = [];
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $prodi = $_POST['prodi296'];
+      $angkatan = $_POST['angkatan296'];
+      $semester = $_POST['semester296'];
+      $jumlah_mk = $_POST['jumlah_mk296'];
+      $ipk = $_POST['ipk296'];
+      $kelas = $_POST['kelas296'];
+      $dospem = $_POST['dospem296'];
+
+      $dataInput = new detailAkademik296($prodi, $angkatan, $semester, $jumlah_mk, $ipk, $kelas, $dospem);
+
+      $data[] = $dataInput;
+
+      echo "<h2 class='mt-5 text-center'>Data Akademik</h2>";
+      echo "<table class='table table-striped mt-3'>";
+      echo "<thead class='thead-dark text-center'>";
+      echo "<tr><th>Program Studi</th><th>Dospem</th><th>Kelas</th><th>Angkatan</th><th>Semester</th><th>Jumlah Mata Kuliah</th><th>IPK</th></tr>";
+      echo "</thead><tbody>";
+      foreach ($data as $inputData) {
+        echo "<tr>";
+        echo "<td class='text-center'>" . $inputData->getProdi296() . "</td>";
+        echo "<td class='text-center'>" . $inputData->getDospem296() . "</td>";
+        echo "<td class='text-center'>" . $inputData->getKelas296() . "</td>";
+        echo "<td class='text-center'>" . $inputData->getAngkatan296() . "</td>";
+        echo "<td class='text-center'>" . $inputData->getSemester296() . "</td>";
+        echo "<td class='text-center'>" . $inputData->getJumlahMk296() . "</td>";
+        echo "<td class='text-center'>" . $inputData->getIpk296() . "</td>";
+        echo "</tr>";
+      }
+      echo "</tbody></table>";
+    }
+    ?>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -5,16 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-    <title>Document</title>
+    <title>SISTEM AKADEMIK SEDERHANA</title>
 </head>
 
 <body>
     <nav class="navbar bg-primary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="?page=homepage">Navbar</a>
-            <a href="?page=form-data-mahasiswa" class="load-content navbar-brand">Load Page 1</a>
-            <a href="?page=form-data-akademik" class="load-content navbar-brand">Load Page 2</a>
+            <a class="navbar-brand text-white" href="?page=homepage">Navbar</a>
+            <a href="?page=form-data-mahasiswa" class="text-white load-content navbar-brand">Data Mahasiswa</a>
+            <a href="?page=form-data-akademik" class="load-content text-white navbar-brand">Data Akademik</a>
         </div>
     </nav>
     <div id="main-content" class="mx-auto p-4">
@@ -26,19 +25,26 @@
             } elseif ($page == 'form-data-akademik') {
                 include 'formAkademik.php';
             } elseif ($page == 'homepage') {
-                echo '<h1>Welcome to the Home Page</h1>';
-                echo '<p>This is the default content.</p>';
-            }
-            else {
+                echo '<h1>Welcome to Sistem Akademik Sederhana</h1>';
+                echo '<p>segera isi data diri anda!!</p>';
+            } else {
                 echo '<h1>Page Not Found</h1>';
                 echo '<p>The page you are looking for does not exist.</p>';
             }
         } else {
-            echo '<h1>Welcome to the Home Page</h1>';
-            echo '<p>This is the default content.</p>';
+            echo '<h1>Welcome to Sistem Akademik Sederhana</h1>';
+            echo '<p>segera isi data diri anda!!</p>';
         }
         ?>
     </div>
+    <footer class="bg-body-tertiary text-center text-lg-start fixed-bottom">
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+            © 2024 Copyright:
+            <a class="text-body mx-2" href="#">Dimah Luthfi</a>
+            <a class="text-body mx-2" href="#">Hadyan Saputra</a>
+        </div>
+    </footer>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
